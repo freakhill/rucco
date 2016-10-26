27f1aba3e0d0294f6ed18123dbc168a7530ee5b3
johan gall <johan.gall@gmail.com>
2016-10-26 16:50:23 +0900

    stuff

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,22 @@ extern crate clap;
 use clap::{Arg, App, SubCommand};
 use std::collections::HashMap;
 
+//#[derive(Debug)]
+struct Args<'a> {
+    conf: Option<&'a str>,
+    output: Option<&'a str>,
+    recursive: Option<bool>,
+    inputs: Vec<&'a str>
+}
+
 const ABOUT: &'static str = "
 Rucco, a docco derivative (documentation generator).
 
 This tool will automatically generate a 'Ruccofile.toml' conf file if lacking.
 ";
 
-fn parse_args() {
-    let matches = App::new("rucco")
+fn parse_args<'a, 'b>(app : &'b mut App<'a, 'b>) -> Args<'a> {
+    let matches = app
         .version("1.0")
         .author("jojo <gall.johan@linecorp.com>")
         .about(ABOUT)
@@ -31,11 +39,10 @@ fn parse_args() {
              .value_name("TARGETDIR")
              .help("Output directory (default is \"docs\")")
              .takes_value(true))
-        .arg(Arg::with_name("recursive")
-             .short("r")
-             .long("recursive")
+        .arg(Arg::with_name("notrecursive")
+             .long("not-recursive")
              .value_name("FILE")
-             .help("Explore directories recursively (default is true)")
+             .help("Do not explore directories recursively (recurse by default)")
              .takes_value(true))
         .arg(Arg::with_name("inputs")
              .help("Files and directories to parse for documentation")
@@ -44,23 +51,13 @@ fn parse_args() {
              .index(1))
         .get_matches();
 
-    // Gets a value for config if supplied by user, or defaults to "default.conf"
-    let inputs = matches.values_of("inputs").unwrap();
-    for s in inputs {
-        println!("Value for config: {}", s);
-    }
+    let inputs : Vec<&str> = matches.values_of("inputs").unwrap().collect();
 
-    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-    // required we could have used an 'if let' to conditionally get the value)
-    //println!("Using input file: {}", matches.value_of("INPUT").unwrap());
-
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
+    Args {
+        conf: matches.value_of("config"),
+        output: matches.value_of("output"),
+        recursive: matches.value_of("recursive"),
+        inputs: inputs,
     }
 }
 
@@ -73,7 +70,8 @@ fn main() {
     // 4.2 use syntect to colorize code (html)
     // 4.3 stick in between comment section
     // 4.4 render the whole thing with regex
-    let args = parse_args();
+    let app = App::new("rucco");
+    let args = parse_args(&mut app);
     let files: HashMap<Vec<u8>, Vec<u8>> = embed!("resources");
     //for (name, content) in files {
     //    println!("{}: \"{}\"", String::from_utf8(name).unwrap(), String::from_utf8(content).unwrap().trim());
