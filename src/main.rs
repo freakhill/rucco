68914ec31c744d59042a5217f32beb6c38879297
johan gall <johan.gall@gmail.com>
2016-10-28 01:33:39 +0900

    not working but dont want to lose the code

--- src/main.rs
+++ src/main.rs
@@ -26,17 +26,21 @@
 extern crate env_logger; /// makes our logger configurable by environment variable (eg. RUST_LOG=debug)
 extern crate toml; /// for configuration files
 extern crate clap; /// "Command Line Argument Parsing" library
+extern crate walkdir; /// to... walk dirs
 
 use clap::{Arg, ArgMatches, App};
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::BTreeMap;
+use walkdir
 use std::collections::LinkedList;
 use std::fs::File;
 use std::path::{Path, PathBuf};
 use std::io::prelude::*;
 use std::io;
 use std::fs;
+use std::env;
+use walkdir::Walkdir;
 
 /// ## Static data
 
@@ -197,9 +201,22 @@ fn ensure_ruccofile_exists(config: &Config) -> Result<(), io::Error> {
 
 /// ## The function actually doing stuff
 
+/// We transform quite early paths to absolute paths so unless specifically mentionned
+/// we will be dealing with absolute paths!
 
-fn ensure_dir(config: &Config, dir: &PathBuf) {
+fn translate_to_output_dir(output_dir: &PathBuf, pwd: &PathBuf, ) -> PathBuf {
+
+}
+//fn doc_path_for_source_file(pb: &)
+///
+
+
+fn ensure_dir(output_dir: &Config, pwd: &PathBuf, dir: &PathBuf) {
     info!("ensuring dir for: {}", dir.to_str().unwrap_or("`unprintable`"));
+    //
+}
+
+fn ensure_output_dir(config: &Config) {
     if let Err(e) = do_ensure_dir_internal(Path::new(config.output_dir)) {
         panic!("failed to ensure that output directory exists: {}", e);
     };
@@ -212,8 +229,32 @@ fn do_ensure_dir_internal(path: &Path) -> io::Result<()> {
     Ok(())
 }
 
-fn process_file(config: &Config, pb: &PathBuf) {
+fn process_file(config: &Config, pwd: &PathBuf, pb: &PathBuf) {
     info!("processing file: {}", pb.to_str().unwrap_or("`unprintable`"));
+}
+
+trait Absolute {
+    fn to_absolute(&self, &PathBuf) -> PathBuf;
+}
+
+impl Absolute for Path {
+    fn to_absolute(&self, pwd: &PathBuf) -> PathBuf {
+        if !self.is_absolute() {
+            pwd.join(self)
+        } else {
+            self.to_owned()
+        }
+    }
+}
+
+impl Absolute for PathBuf {
+    fn to_absolute(&self, pwd: &PathBuf) -> PathBuf {
+        if !self.is_absolute() {
+            pwd.join(self)
+        } else {
+            self.clone()
+        }
+    }
 }
 
 /// ## The main function!
@@ -284,45 +325,63 @@ fn main() {
     }
 
     // and now recurse files and dump shit!
-    enum Action {
-        EnsureDir(Box<PathBuf>),
-        ProcessFile(Box<PathBuf>)
-    };
+    let pwd = env::current_dir().expect("failed to get current dir!");
+    let absolute_output_dir = PathBuf::from(config.output_dir).to_absolute(&pwd);
+
+    let entries = config.entries.iter().map(|ref p| PathBuf::new(p));
+    if config.recursive {
+        for entry in entries {
+            for entry in WalkDir::new(entry).filter_map(|p| p.is_ok()) {
+                println!("{}", entry.path().display());
+            }
+        }
+    } else {
+        for file in entries.filter_map(|p| p.is_file()) {
+            println!("{}", file.display());
+        }
+    }
     // don't blow the stack (ノ°Д°）ノ︵ plzplz!
     // simple recursion, we use a struct to provide ~~anonymous~~ recursion
     struct Dive<'s> { f: &'s Fn(&Dive, Box<PathBuf>, u8) -> LinkedList<Action> };
     let dive: Dive = Dive { f: &|dive, p, depth_allowed| {
         let mut l: LinkedList<Action> = LinkedList::new();
-        let path = p.as_path();
-        if path.is_file() {
-            l.push_back(Action::ProcessFile(p.clone()));
-        } else if path.is_dir() && depth_allowed != 0 {
-            l.push_back(Action::EnsureDir(p.clone()));
-            match path.read_dir() {
-                Ok(dir) => {
-                    for entryres in dir {
-                        match entryres {
-                            Ok(entry) => {
-                                let mut segment = (dive.f)(&dive, Box::new(entry.path().to_owned()), depth_allowed - 1);
-                                l.append(&mut segment);
-                            },
-                            Err(e) => {error!("failed to read dir entry in dir {}: {}", path.to_str().unwrap_or("`unprintable`"), e);}
+        if p.starts_with(absolute_output_dir) {
+            // we skip on dirs and files inside our ouput directory!
+            l
+        } else {
+            let path = p.as_path();
+            if path.is_file() {
+                l.push_back(Action::ProcessFile(p.clone()));
+            } else if path.is_dir() && depth_allowed != 0 {
+                l.push_back(Action::EnsureDir(p.clone()));
+                match path.read_dir() {
+                    Ok(dir) => {
+                        for entryres in dir {
+                            match entryres {
+                                Ok(entry) => {
+                                    let mut segment = (dive.f)(&dive, Box::new(entry.path().to_owned()), depth_allowed - 1);
+                                    l.append(&mut segment);
+                                },
+                                Err(e) => {error!("failed to read dir entry in dir {}: {}", path.to_str().unwrap_or("`unprintable`"), e);}
+                            }
                         }
-                    }
-                },
+                    },
                 Err(e) => {error!("failed to read dir {}: {}", path.to_str().unwrap_or("`unprintable`"), e);}
+                };
             };
-        };
-        l
+            l
+        }
     }};
     info!("generating list of files to process.");
-    let actions = config.entries.iter().flat_map(|ref p| (dive.f)(&dive, Box::new(Path::new(p).to_owned()), MAX_RECURSE));
+    let actions = config.entries.iter().flat_map(|ref p| (dive.f)(&dive, Box::new(PathBuf::from(p).to_absolute(&pwd)), MAX_RECURSE));
+    info!("ensure output dir exists");
+    ensure_output_dir(&config);
     info!("processing files");
     for action in actions {
         match action {
-            Action::EnsureDir(dir) => { ensure_dir(&config, &dir); },
-            Action::ProcessFile(file) => { process_file(&config, &file); }
+            Action::EnsureDir(dir) => { ensure_dir(&absolute_output_dir, &pwd, &dir); },
+            Action::ProcessFile(file) => { process_file(&config, &absolute_output_dir, &pwd, &file); }
         }
-    }
+    };
     info!("complete!");
 }
